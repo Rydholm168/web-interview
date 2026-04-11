ce017fe0a0a0bdfe89848aa12efd2089e9792a04
finalManu <manuelrydholm@gmail.com>
2026-04-11 20:15:32 +0200

    Font & update favicon

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import { ApolloProvider } from '@apollo/client/react'
+import { CssBaseline, GlobalStyles, ThemeProvider, createTheme } from '@mui/material'
 import App from './App'
 
 const client = new ApolloClient({
@@ -9,9 +10,24 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'ballinger, sans-serif',
+    button: { fontFamily: 'ballinger, sans-serif' },
+  },
+})
+
 const root = createRoot(document.getElementById('root'))
 root.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <GlobalStyles
+      styles={{
+        body: { fontFamily: 'ballinger, sans-serif' },
+      }}
+    />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </ThemeProvider>
 )
